d1f223c8a1ee63a3fcae3ea48e7329ad5f532170
Frediy3 <fredygithub77@gmail.com>
2025-07-11 11:37:53 -0400

    Feature: Create post

---
+++ main.js
@@ -0,0 +1,13 @@
+const formulario = document.querySelector(".form-create");
+
+formulario.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const publicacion = document.querySelector("#publicacion").value;
+    const contenedor = document.querySelector(".contenedor-publicacion");
+    contenedor.innerHTML += `<div class="publicacion">
+            <p>id: 00</p>
+            <p>${publicacion}</p>
+            <button>editar</button>
+            <button>eliminar</button>
+        </div>`;
+});
