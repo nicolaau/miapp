eb28892ea7ec535f686b1f7982254fe4c9f83c64
KSDR-CXEL <carlosjoelsantistebanquevedo@gmail.com>
2025-07-14 15:16:58 -0400

    wea pencaa

--- main.js
+++ main.js
@@ -1,13 +1,14 @@
 const formulario = document.querySelector(".form-create");
-
+let contador = 0;
 formulario.addEventListener("submit", (e) => {
     e.preventDefault();
     const publicacion = document.querySelector("#publicacion").value;
     const contenedor = document.querySelector(".contenedor-publicacion");
     contenedor.innerHTML += `<div class="publicacion">
-            <p>id: 00</p>
+            <p>id: ${contador}</p>
             <p>${publicacion}</p>
             <button>editar</button>
             <button>eliminar</button>
         </div>`;
+        contador++;
 });
